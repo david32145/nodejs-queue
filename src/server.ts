2fc24441123acbb3a3fd1f52d1ac0e7ad3120325
david32145 <nascimento32145@gmail.com>
2020-06-15 19:51:41 -0300

    Add mail and report routes

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,40 @@ import express from "express"
 
 const app = express()
 
+app.use(express.json())
+
+app.get("/mail", async (req, res) => {
+  const body = req.body
+  await MailQueue.enqueue({
+    content: String(body.content),
+    from: String(body.from),
+    to: String(body.to),
+  })
+
+  return res.status(200).json({
+    content: String(body.content),
+    from: String(body.from),
+    to: String(body.to),
+  })
+})
+
+app.get("/report", async (req, res) => {
+  const body = req.body
+  await ReportQueue.enqueue({
+    title: String(body.title),
+    description: String(body.description),
+  })
+
+  return res.status(200).json({
+    title: String(body.title),
+    description: String(body.description),
+  })
+})
+
 const SERVER_PORT = process.env.PORT || 3333
 
-app.listen(SERVER_PORT, function(...err: any[]) {
-  if(err && err.length) {
+app.listen(SERVER_PORT, function (...err: any[]) {
+  if (err && err.length) {
     console.log('An error ocurrent...')
     console.error(err)
   } else {
