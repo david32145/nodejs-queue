c1a28ed5a250546afb13f3ba63a0d6608d643f7f
david32145 <nascimento32145@gmail.com>
2020-06-15 17:39:38 -0300

    Config redis

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("here")
+import * as redis from "redis"
+
+const client = redis.createClient({
+  port: Number(process.env.REDIS_PORT),
+  host: process.env.REDIS_HOST
+})
+
+client.on("connect", function(...arg: any[]) {
+  console.log("connected")
+})
