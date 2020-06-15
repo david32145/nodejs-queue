ae851f7e9d83a5e5ad1184cc41c2dc5d0ffe6f3d
david32145 <nascimento32145@gmail.com>
2020-06-15 19:48:09 -0300

    Init express app

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-import "../config"
+import "./config/env"
+
+import MailQueue from "./queues/Mail"
+import ReportQueue from "./queues/Report"
+
+import express from "express"
+
+const app = express()
+
+const SERVER_PORT = process.env.PORT || 3333
+
+app.listen(SERVER_PORT, function(...err: any[]) {
+  if(err && err.length) {
+    console.log('An error ocurrent...')
+    console.error(err)
+  } else {
+    console.log(`Server is running in http://localhost:${SERVER_PORT}`)
+  }
+})
